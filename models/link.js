784b9c8ccdf43cf66527542e5bfb0ae9db615e48
aaronsato <2asato@gmail.com>
2020-03-17 12:34:38 -0700

    added link schema

--- models/link.js
+++ models/link.js
@@ -1,23 +1,23 @@
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-// // schema
-// var linkSchema = new mongoose.Schema({
-//     name: String,
-//     author: {
-//         id: {
-//             type: mongoose.Schema.Types.ObjectId,
-//             ref: 'Link'
-//         },
-//         username: String
-//     },
-//     site: String,
-//     description: String,
-//     image: String,
-//     added: { type: Date, default: Date.now }
-// });
+// schema
+var linkSchema = new mongoose.Schema({
+    name: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Link'
+        },
+        username: String
+    },
+    site: String,
+    description: String,
+    image: String,
+    added: { type: Date, default: Date.now }
+});
 
-// // link model
-// module.exports = mongoose.model('Link', linkSchema);
+// link model
+module.exports = mongoose.model('Link', linkSchema);
 
 
 
