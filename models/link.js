78cdab83cb9b60003b934c155ef71e2c4443bffb
aaronsato <2asato@gmail.com>
2020-03-17 08:39:25 -0700

    readded author to links

--- models/link.js
+++ models/link.js
@@ -2,6 +2,13 @@ var mongoose = require('mongoose');
 
 // schema
 var linkSchema = new mongoose.Schema({
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
     name: String,
     site: String,
     description: String,
